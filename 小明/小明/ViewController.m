44d09e61ff974ed26bed4edf42d68a4915ad2b30
peter <peter@hotmail.com>
2016-02-21 18:49:21 +0800

    xiaoming

---
+++ 小明/小明/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  小明
+//
+//  Created by tarena on 16/2/21.
+//  Copyright © 2016年 MFCompany. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UIButton *btn = [UIButton buttonWithType:0];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
